a76a2f4748e9316ee9fc61c4169d266ee15749f7
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-01-26 14:03:39 +0700

    deploy ke-1

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,89 @@
+const ClientError = require("../../exceptions/ClientError");
+
+class CollaborationsHandler {
+  constructor(service, validator) {
+    const { collaborationsService, playlistsService, usersService } = service;
+    this._service = collaborationsService;
+    this._playlistsService = playlistsService;
+    this._usersService = usersService;
+    this._validator = validator;
+
+    this.postCollaborationHandler = this.postCollaborationHandler.bind(this);
+    this.deleteCollaborationHandler = this.deleteCollaborationHandler.bind(this);
+  }
+
+  async postCollaborationHandler(request, h) {
+    try {
+      this._validator.validateCollaborationPayload(request.payload);
+      const { id: credentialId } = request.auth.credentials;
+      const { playlistId, userId } = request.payload;
+
+      await this._playlistsService.getPlaylistsById(playlistId);
+      await this._usersService.getUserById(userId);
+
+      await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+      const collaborationId = await this._service.addCollaboration(playlistId, userId);
+      const response = h.response({
+        status: "success",
+        message: "Kolaborasi berhasil ditambahkan",
+        data: {
+          collaborationId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Mohon maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.log(error);
+      return response;
+    }
+  }
+
+  async deleteCollaborationHandler(request, h) {
+    try {
+      this._validator.validateCollaborationPayload(request.payload);
+      const { id: credentialId } = request.auth.credentials;
+      const { playlistId, userId } = request.payload;
+      await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+      await this._service.deleteCollaboration(playlistId, userId);
+      return {
+        status: "success",
+        message: "Kolaborasi berhasil dihapus",
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Mohon maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.log(error);
+      return response;
+    }
+  }
+}
+
+module.exports = CollaborationsHandler;
---
+++ src/utils/index.js
@@ -0,0 +1,29 @@
+const mapDBToModelAlbums = ({
+  id,
+  name,
+  year,
+}) => ({
+  id,
+  name,
+  year,
+});
+
+const mapDBToModelSongs = ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  album_id,
+}) => ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = { mapDBToModelAlbums, mapDBToModelSongs };
